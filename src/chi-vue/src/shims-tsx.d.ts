aa266e4b0588bd805e1979b7d001daa2ecc80e11
Cristina Torres <cristina.torres@lumen.com>
2023-06-08 16:36:03 +0200

    tsx props types

--- src/chi-vue/src/shims-tsx.d.ts
+++ src/chi-vue/src/shims-tsx.d.ts
@@ -1,16 +1,8 @@
-import Vue, { VNode } from 'vue';
-
-declare global {
-  namespace JSX {
-    // tslint:disable no-empty-interface
-    interface Element extends VNode {}
-    // tslint:disable no-empty-interface
-    interface ElementClass extends Vue {}
-    interface IntrinsicElements {
-      [elem: string]: any;
-    }
-    interface ElementAttributesProperty {
-      $props: {};
-    }
+declare module 'vue' {
+  interface ComponentCustomProps {
+    message?: string;
   }
 }
+
+export {}
+
